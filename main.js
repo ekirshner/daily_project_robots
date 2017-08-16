7919dfd3384723d1d2e0aade2fa616ff5815c009
Erica Kirshner <ekkicks@gmail.com>
2017-08-15 22:30:36 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+
+const data = require('./data.js');
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.get('/', function(req,res) {
+    res.render('index.mustache', { users: data.users})
+});
+
+// creating localhost
+app.listen(3000, function () {
+    console.log('successfully did shit');
+});
